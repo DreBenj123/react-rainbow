d96e8d4700985aa474977099c4c4def4c8be5ce4
DreBenj123 <100318209+DreBenj123@users.noreply.github.com>
2022-07-28 12:07:24 -0400

    help with importing colorform

---
+++ src/ColorForm.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+export default function ColorForm(props) {
+  let [input, setInput] = useState("");
+
+  return (
+    <div>
+      <form>
+        <input type="text" onChange={(e) => setInput(e.target.value)} />
+        <button type="submit">Submit!</button>
+      </form>
+    </div>
+  );
+}
